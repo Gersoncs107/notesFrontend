0591bff58854367635b0d211898443fa219f7b57
Gersoncs107 <gerson.silva107@gmail.com>
2025-08-09 19:53:47 -0300

    notes-frontend-update

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import AppTest from "./AppTest"
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AppTest/>
+)
