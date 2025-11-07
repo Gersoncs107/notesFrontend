4c640256fade3d795dcb95fbf794216d1a58c812
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-06 22:32:23 -0300

    notes-frontend-update

--- src/App.jsx
+++ src/App.jsx
@@ -93,6 +93,40 @@ const App = () => {
       }
     }
 
+     const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>  
+  )
+
 return (
     <div>
         <h1>Notes</h1>
