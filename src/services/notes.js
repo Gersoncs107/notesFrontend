9de64547cc71109e02f37097951f66f975c46193
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-08 22:01:36 -0300

    notes-frontend-update

--- src/services/notes.js
+++ src/services/notes.js
@@ -12,14 +12,14 @@ const getAll = () => {
   return request.then((response) => response.data);
 };
 
-const create = async (newObject) => {
+const create = async newObject => {
   const config = {
     headers: { Authorization: token },
   }
 
-  const response = axios.post(baseUrl, newObject, config);
+  const response = await axios.post(baseUrl, newObject, config)
   return response.data
-};
+}
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject);
