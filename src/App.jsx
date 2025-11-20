5aa913711d9549390df9b6e520b696dfddf63d05
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-20 18:30:20 -0300

    notes-frontend-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
