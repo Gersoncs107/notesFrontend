797c85703c5e17f2e4527c22e702fa0caec8d078
Gersoncs107 <gerson.silva107@gmail.com>
2025-12-04 19:48:54 -0300

    notes-frontend-update

--- src/components/note.test.js
+++ src/components/note.test.js
@@ -38,34 +38,14 @@ test('clicking the button calls event handler once', async () => {
   expect(mockHandler.mock.calls).toHaveLength(1)
 })
 
-describe('<Togglable />', () => {
-  let container
-
-  beforeEach(() => {
-    container = render(
-      <Togglable buttonLabel="show...">
-        <div className="testDiv" >
-          togglable content
-        </div>
-      </Togglable>
-    ).container
-  })
-
-  test('renders its children', async () => {
-    await screen.findAllByText('togglable content')
-  })
-
-  test('at start the children are not displayed', () => {
-    const div = container.querySelector('.togglableContent')
-    expect(div).toHaveStyle('display: none')
-  })
-
-  test('after clicking the button, children are displayed', async () => {
+ test('toggled content can be closed', async () => {
     const user = userEvent.setup()
     const button = screen.getByText('show...')
     await user.click(button)
 
+    const closeButton = screen.getByText('cancel')
+    await user.click(closeButton)
+
     const div = container.querySelector('.togglableContent')
-    expect(div).not.toHaveStyle('display: none')
+    expect(div).toHaveStyle('display: none')
   })
-})
