cad77e4fa998a41a799a986ec625e78987c425ba
Gersoncs107 <gerson.silva107@gmail.com>
2025-12-25 19:21:06 -0300

    notes-frontend-update

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,12 +1,12 @@
 describe('Note app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:5173/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     const user = {
       name: 'Superuser',
       username: 'root',
       password: 'salainen'
     }
-    cy.request('POST', 'http://localhost:5173/api/users/', user)
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
     cy.visit('http://localhost:5173/')
   })
 
